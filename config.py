6f7d909b6ca0d0811a4e6a8002ccd53c08e8c19f
voidnologo <(none)>
2019-12-30 21:07:04 -0500

    Chapter 04: db

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'my_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app.db")}')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
