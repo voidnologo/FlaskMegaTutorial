a6012e3fe58e5ee1e8959569d7b017fa2e69cf7e
voidnologo <(none)>
2020-01-01 13:59:41 -0500

    Chapter 07: errors

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,9 @@
 FLASK_APP=microblog.py
 FLASK_ENV=development
+
+
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=reboot.yourself@gmail.com
+MAIL_PASSWORD=<your-gmail-password>
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -14,4 +18,47 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+def email_logger():
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=f'no-reply@{app.config["MAIL_SERVER"]}',
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+def file_logger():
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:(lineno)d]'
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+
+if not app.debug:
+    # email ERROR level issues
+    email_logger()
+    # file logger for INFO+ issues
+    file_logger()
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
+from app import routes, models, errors
