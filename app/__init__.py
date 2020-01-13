c1295554abad60b557d80c93c4841feecc6d056c
voidnologo <(none)>
2020-01-13 17:58:57 -0500

    Chapter 12, time

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -37,6 +38,9 @@ mail = Mail(app)
 
 # css/js
 bootstrap = Bootstrap(app)
+
+# time
+moment = Moment(app)
 
 
 def email_logger():
